2009f69c4074fc18016fa9c03d222a64e20bd156
diogommartins <magalhaesmartins@icloud.com>
2017-04-24 00:07:03 -0300

    bumping version and updating the setup

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ BASE_PATH = os.path.dirname(__file__)
 setup(
     name='easyqueue',
     version=__version__,
+    author='Diogo Magalhães Martins',
+    author_email='magalhaesmartins@icloud.com',
+    maintainer='Sieve',
+    maintainer_email='ti@sieve.com.br',
+    description="An easy way to handle amqp queues. Focus on your app logic, "
+                "forget about the protocol.",
     url='https://bitbucket.org/sievetech/easyqueue',
     packages=find_packages(exclude=['easyqueue/tests']),
     install_requires=[
