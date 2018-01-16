2b7937f55bd12dd74318945b78f2856f29b7607a
Douglas Bastos <douglashsb@gmail.com>
2018-01-16 16:14:46 -0200

    Pequena refatoração

--- easyqueue/queue.py
+++ easyqueue/queue.py
@@ -53,8 +53,8 @@ class BaseQueue(metaclass=abc.ABCMeta):
 class BaseJsonQueue(BaseQueue):
     content_type = 'application/json'
 
-    def serialize(self, body: Any, ensure_ascii: bool=True) -> str:
-        return json.dumps(body, ensure_ascii=ensure_ascii)
+    def serialize(self, body: Any, **kwargs) -> str:
+        return json.dumps(body, **kwargs)
 
     def deserialize(self, body: str) -> Any:
         return json.loads(body)
