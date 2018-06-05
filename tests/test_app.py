32961861da98085407dc86919c76f10948966a7e
Dalton Barreto <daltonmatos@gmail.com>
2018-06-05 16:15:37 -0300

    @app.route() recebe uma lista de routes
    IN-2511
    
    Agora podemos registra um mesmo handler em múiltiplas rotas sem precisar
    de wrappers.

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from worker import App
 class AppTest(unittest.TestCase):
 
     def test_register_hander_on_route_registry(self):
-        expected_route = "/asgard/counts/ok"
+        expected_route = ["/asgard/counts/ok"]
         expected_vhost = "/"
         app = App()
         @app.route(expected_route, vhost=expected_vhost)
@@ -15,11 +15,39 @@ class AppTest(unittest.TestCase):
 
         self.assertIsNotNone(app.routes_registry)
         expected_registry_entry = {
-            "route": expected_route,
+            "route": expected_route[0],
             "handler": _handler,
             "options": {
                 "vhost": expected_vhost
             }
         }
         self.assertEqual(expected_registry_entry, app.routes_registry['/asgard/counts/ok'])
+        self.assertEqual(42, asyncio.get_event_loop().run_until_complete(_handler(None)))
+
+    def test_register_list_of_routes_to_the_same_handler(self):
+        expected_routes = ["/asgard/counts/ok", "/asgard/counts/errors"]
+        expected_vhost = "/"
+        app = App()
+        @app.route(expected_routes, vhost=expected_vhost)
+        async def _handler(message):
+            return 42
+
+        self.assertIsNotNone(app.routes_registry)
+        expected_ok_registry_entry = {
+            "route": expected_routes[0],
+            "handler": _handler,
+            "options": {
+                "vhost": expected_vhost
+            }
+        }
+
+        expected_errors_registry_entry = {
+            "route": expected_routes[1],
+            "handler": _handler,
+            "options": {
+                "vhost": expected_vhost
+            }
+        }
+        self.assertEqual(expected_ok_registry_entry, app.routes_registry['/asgard/counts/ok'])
+        self.assertEqual(expected_errors_registry_entry, app.routes_registry['/asgard/counts/errors'])
         self.assertEqual(42, asyncio.get_event_loop().run_until_complete(_handler(None)))
--- worker/__init__.py
+++ worker/__init__.py
@@ -13,14 +13,15 @@ class App:
     def __init__(self):
         self.routes_registry = {}
 
-    def route(self, route, vhost):
+    def route(self, routes, vhost):
         def wrap(f):
-            self.routes_registry[route] = {
-                "route": route,
-                "handler": f,
-                "options": {
-                    "vhost": vhost,
+            for route in routes:
+                self.routes_registry[route] = {
+                    "route": route,
+                    "handler": f,
+                    "options": {
+                        "vhost": vhost,
+                    }
                 }
-            }
             return f
         return wrap
