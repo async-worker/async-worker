cf6f8edd070fccd906e5b625206d5aec7c1a429b
Dalton Barreto <daltonmatos@gmail.com>
2018-06-07 11:12:47 -0300

    worker.App já passando paramertos de conexão
    IN-2511

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,6 +1,10 @@
 import functools
 import asyncio
 
+from worker.consumer import Consumer
+
+from countsingestor import conf
+
 def entrypoint(f):
     @functools.wraps(f)
     def _(*args, **kwargs):
@@ -10,8 +14,12 @@ def entrypoint(f):
 
 
 class App:
-    def __init__(self):
+    def __init__(self, host, user, password, prefetch_count):
         self.routes_registry = {}
+        self.host = host
+        self.user = user
+        self.password = password
+        self.prefetch_count = prefetch_count
 
     def route(self, routes, vhost="/"):
         def wrap(f):
@@ -24,3 +32,20 @@ class App:
             }
             return f
         return wrap
+
+    def _build_consumers(self):
+        consumers = []
+        for _handler, route_info in self.routes_registry.items():
+            consumers.append(Consumer(route_info, self.host, self.user, self.password, self.prefetch_count))
+        return consumers
+
+    @entrypoint
+    async def run(self):
+        conf.logger.info("Booting App...")
+        consumers = self._build_consumers()
+        for consumer in consumers:
+            asyncio.get_event_loop().create_task(consumer.start())
+        while True:
+            await asyncio.sleep(10)
+
+
