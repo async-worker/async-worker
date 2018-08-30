2fec2a820e3843d1566169dc1525aecae3670a11
Dalton Barreto <daltonmatos@gmail.com>
2018-08-30 13:42:58 -0300

    Version bump. 0.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='async-worker',
-    version='0.4.0-rc1',
+    version='0.4.0',
     description="Microframework para escrever consumers para RabbitMQ",
     long_description="Microframework para escrever consumers para RabbitMQ",
     url='https://github.com/B2W-BIT/async-worker',
