6017135fbeb9c474dec0b9680b4e5525ecb14065
diogommartins <magalhaesmartins@icloud.com>
2018-11-11 17:01:49 -0200

    Movendo gerenciamento de configuração para pydantic

--- asyncworker/conf.py
+++ asyncworker/conf.py
@@ -1,9 +1,19 @@
-import os
 import logging
 
+from pydantic import BaseSettings
 from simple_json_logger import JsonLogger
 
-LOGLEVEL = os.getenv("ASYNCWORKER_LOGLEVEL", "ERROR")
+
+class Settings(BaseSettings):
+    LOGLEVEL: str = "ERROR"
+    HTTP_ENABLED: bool = True
+
+    class Config:
+        allow_mutation = False
+        env_prefix = "ASYNCWORKER_"
+
+
+settings = Settings()
 
 logger = JsonLogger(flatten=True)
-logger.setLevel(getattr(logging, LOGLEVEL, logging.INFO))
+logger.setLevel(getattr(logging, settings.LOGLEVEL, logging.INFO))
