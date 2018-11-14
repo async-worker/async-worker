3610abfec237ea56d4aa9ab6e6d26f51834534d1
diogommartins <magalhaesmartins@icloud.com>
2018-11-14 21:42:58 -0200

    Refactoring routes system

--- asyncworker/models.py
+++ asyncworker/models.py
@@ -1,8 +1,9 @@
 from collections import UserDict
-from typing import Callable, Coroutine, Dict
+from typing import Callable, Coroutine, Dict, List
 
-from yarl import cached_property
+from cached_property import cached_property
 
+from asyncworker.options import Defaultvalues, Events, Options
 
 Route = Callable[[], Coroutine]
 
@@ -13,10 +14,27 @@ class Routes(UserDict):
         self._cache = {}
 
     @cached_property
-    def amqp_routes(self) -> Dict[Route, Dict]:
-        routes = {}
+    def amqp_routes(self) -> List[Dict]:
+        routes = []
         for handler, route in self.items():
             if route['type'] == 'amqp':
-                routes[handler] = dict(**route)
-                routes[handler].pop('type')
+                options = route['options']
+                routes.append({
+                    "routes": route['routes'],
+                    "handler": handler,
+                    "options": {
+                        "vhost": route['vhost'],
+                        "bulk_size": options.get(Options.BULK_SIZE,
+                                                 Defaultvalues.BULK_SIZE),
+                        "bulk_flush_interval": options.get(
+                            Options.BULK_FLUSH_INTERVAL,
+                            Defaultvalues.BULK_FLUSH_INTERVAL),
+                        Events.ON_SUCCESS: options.get(
+                            Events.ON_SUCCESS,
+                            Defaultvalues.ON_SUCCESS),
+                        Events.ON_EXCEPTION: options.get(
+                            Events.ON_EXCEPTION,
+                            Defaultvalues.ON_EXCEPTION),
+                    }
+                })
         return routes
