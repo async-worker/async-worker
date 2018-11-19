59601fd56d88facee613bba57fe433d6b89e26f5
diogommartins <magalhaesmartins@icloud.com>
2018-11-19 16:45:14 -0200

    Adding HTTPServer signal handler for declaring http routes

--- asyncworker/__init__.py
+++ asyncworker/__init__.py
@@ -1,11 +1,12 @@
 from asyncworker.base import BaseApp
 from asyncworker.consumer import Consumer
 from asyncworker.options import Options, Defaultvalues, Events
+from asyncworker.signals.handlers.http import HTTPServer
 from asyncworker.signals.handlers.rabbitmq import RabbitMQ
 
 
 class App(BaseApp):
-    handlers = (RabbitMQ(),)
+    handlers = (RabbitMQ(), HTTPServer())
 
     def __init__(self, host, user, password, prefetch_count):
         super(App, self).__init__()
