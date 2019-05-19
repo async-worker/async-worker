b0bffdb04ecb42d816a40b4d36a48ebc02c5d16a
Diogo Magalhães Martins <diogommartins.dev@gmail.com>
2019-05-19 12:38:14 -0300

    #60 Refactoring rabbitmq SignalHandler to use the connections from App

--- asyncworker/signals/handlers/rabbitmq.py
+++ asyncworker/signals/handlers/rabbitmq.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from asyncworker.consumer import Consumer
 from asyncworker.signals.handlers.base import SignalHandler
@@ -6,20 +6,32 @@ from asyncworker.rabbitmq.connection import AMQPConnection
 from asyncworker.options import RouteTypes
 
 if TYPE_CHECKING:  # pragma: no cover
-    from asyncworker import App  # noqa: F401
+    from asyncworker.app import App  # noqa: F401
 
 
 class RabbitMQ(SignalHandler):
     async def startup(self, app: "App"):
-        app[RouteTypes.AMQP_RABBITMQ] = {}
-        app[RouteTypes.AMQP_RABBITMQ]["connection"] = AMQPConnection(
-            hostname=app.host, username=app.user, password=app.password
-        )
         app[RouteTypes.AMQP_RABBITMQ]["consumers"] = []
+        connections: List[AMQPConnection] = app[RouteTypes.AMQP_RABBITMQ][
+            "connections"
+        ]
         for route_info in app.routes_registry.amqp_routes:
+            try:
+                connection = connections[0]
+            except IndexError as e:
+                raise ValueError(
+                    "Invalid route definition for App. You are trying to "
+                    "define a RouteType.AMQP_RABBITMQ without an "
+                    "AMQPConnection registered on App."
+                ) from e
+
             consumer = Consumer(
-                route_info, app.host, app.user, app.password, app.prefetch_count
+                route_info=route_info,
+                host=connection.hostname,
+                username=connection.username,
+                password=connection.password,
+                prefetch_count=connection.prefetch,
             )
             app[RouteTypes.AMQP_RABBITMQ]["consumers"].append(consumer)
-            app[RouteTypes.AMQP_RABBITMQ]["connection"].register(consumer.queue)
+            connection.register(consumer.queue)
             app.loop.create_task(consumer.start())
