0d6af4afbeb15d067e7924dae321bc71e85c6db8
Dalton Barreto <daltonmatos@gmail.com>
2019-07-05 15:42:49 -0300

    Implementação do HttpClientContext

--- tests/test_test_utils.py
+++ tests/test_test_utils.py
@@ -1,8 +1,11 @@
+import asyncio
+import os
+
 from aiohttp import web
 from aiohttp.test_utils import TestClient
-from asynctest import TestCase, skip
+from asynctest import TestCase, mock
 from asyncworker import App, RouteTypes
-from asyncworker.testing import http_client
+from asyncworker.testing import http_client, HttpClientContext
 
 
 class HttpClientTestCaseDecoratorTest(TestCase):
@@ -47,17 +50,46 @@ class HttpClientTestCaseDecoratorTest(TestCase):
         async def index(request):
             return web.json_response({"OK": True})
 
-        @http_client(self.app)
-        async def my_method(http_client):
-            raise Exception("BOOM")
+        with mock.patch.dict(os.environ, TEST_ASYNCWORKER_HTTP_PORT="9999"):
 
-        with self.assertRaises(Exception):
-            await my_method()
+            @http_client(self.app)
+            async def my_method(http_client):
+                raise Exception("BOOM")
 
-        @http_client(self.app)
-        async def other_method(http_client):
+            with self.assertRaises(Exception):
+                await my_method()
+
+            @http_client(self.app)
+            async def other_method(http_client):
+                resp = await http_client.get("/")
+                return await resp.json()
+
+            self.assertEqual({"OK": True}, await other_method())
+
+
+class HttpClientContextManagerTest(TestCase):
+    async def setUp(self):
+        self.app = App("", "", "", 1)
+
+    async def test_client_can_perform_requests(self):
+        @self.app.route(["/"], type=RouteTypes.HTTP, methods=["GET"])
+        async def index(request):
+            return web.json_response({"OK": True})
+
+        async with HttpClientContext(self.app) as http_client:
             resp = await http_client.get("/")
-            return await resp.json()
+            self.assertEqual({"OK": True}, await resp.json())
 
-        self.assertEqual({"OK": True}, await other_method())
+    async def test_can_reuse_port(self):
+        @self.app.route(["/"], type=RouteTypes.HTTP, methods=["GET"])
+        async def index(request):
+            return web.json_response({"OK": True})
 
+        with mock.patch.dict(os.environ, TEST_ASYNCWORKER_HTTP_PORT="9999"):
+            async with HttpClientContext(self.app) as http_client:
+                resp = await http_client.get("/")
+                self.assertEqual({"OK": True}, await resp.json())
+
+            async with HttpClientContext(self.app) as http_client:
+                resp = await http_client.get("/")
+                self.assertEqual({"OK": True}, await resp.json())
