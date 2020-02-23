983c0ad049a59fd9b702d320cd350827e7c27726
Dalton Barreto <daltonmatos@gmail.com>
2020-02-22 21:57:23 -0300

    Adicionando exemplo de handler com path param

--- examples/http_api.py
+++ examples/http_api.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 
 from asyncworker import App, RouteTypes
+from asyncworker.http.decorators import parse_path
 
 app = App()
 
@@ -8,6 +9,12 @@ app = App()
 @app.route(["/", "/other"], type=RouteTypes.HTTP, methods=["GET"])
 async def handler(req: web.Request):
     return web.json_response({})
+
+
+@app.route(["/by_id/{_id}"], type=RouteTypes.HTTP, methods=["GET"])
+@parse_path
+async def by_id(_id: int):
+    return web.json_response({"url-param-value": _id})
 
 
 app.run()
