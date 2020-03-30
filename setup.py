323ef846aea8400285eb792c6a430d748d0cfa6c
Dalton Barreto <daltonmatos@gmail.com>
2020-03-30 16:46:05 -0300

    Version bump to 0.13.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="async-worker",
-    version="0.12.1",
+    version="0.13.0",
     description="Microframework para escrever workers assíncronos em Python",
     long_description="Microframework para escrever workers assíncronos em Python",
     url="https://github.com/b2wdigital/async-worker",
