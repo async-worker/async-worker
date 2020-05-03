7e63bbee84369eb215e607bd077081a88bea250f
diogommartins <diogommartins.dev@gmail.com>
2020-05-03 20:52:13 -0300

    #106 Instrumenting HTTP requests

--- asyncworker/metrics/aiohttp_resources.py
+++ asyncworker/metrics/aiohttp_resources.py
@@ -5,9 +5,35 @@ from aiohttp.web_middlewares import middleware
 
 from asyncworker import metrics
 from asyncworker.conf import default_timer
+from asyncworker.metrics import requests_in_progress
 from asyncworker.metrics.registry import generate_latest
 
 _Handler = Callable[[web.Request], Awaitable[web.Response]]
+
+
+@middleware
+async def http_metrics_middleware(request: web.Request, handler: _Handler):
+    start = default_timer()
+    try:
+        requests_in_progress.labels(
+            method=request.method, path=request.path
+        ).inc()
+        response = await handler(request)
+
+        metrics.request_duration.labels(
+            method=request.method, path=request.path, status=response.status
+        ).observe(default_timer() - start)
+
+        return response
+    except web.HTTPException as e:
+        metrics.request_duration.labels(
+            method=request.method, path=request.path, status=e.status
+        ).observe(default_timer() - start)
+        raise e
+    finally:
+        requests_in_progress.labels(
+            method=request.method, path=request.path
+        ).dec()
 
 
 async def metrics_route_handler(request: web.Request) -> web.Response:
--- asyncworker/metrics/definitions/http.py
+++ asyncworker/metrics/definitions/http.py
@@ -1,9 +1,15 @@
 from asyncworker.conf import INFINITY
-from asyncworker.metrics.types import Histogram
+from asyncworker.metrics.types import Histogram, Gauge
 
 request_duration = Histogram(
     name="asyncworker_http_request_duration",
     documentation="HTTP request duration",
     labelnames=("method", "path", "status"),
     buckets=(0.01, 0.05, 0.1, INFINITY),
+)
+
+requests_in_progress = Gauge(
+    name="asyncworker_http_requests_in_progress",
+    documentation="Count of HTTP requests currently in progress",
+    labelnames=("method", "path"),
 )
