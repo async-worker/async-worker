178ca7ae1a198f485a20fc48893a2897705ccb1c
diogommartins <diogommartins.dev@gmail.com>
2021-04-13 15:56:16 -0300

    #262 Adding a test to validate the wrong use of path label on http metrics

--- tests/metrics/test_http_metrics.py
+++ tests/metrics/test_http_metrics.py
@@ -1,4 +1,5 @@
 import asyncio
+from uuid import uuid4
 from http import HTTPStatus
 
 from aiohttp import ClientSession
@@ -12,6 +13,8 @@ from asyncworker.http.wrapper import RequestWrapper
 
 
 class HTTPMetricsTests(TestCase):
+    app_url = f"http://{settings.HTTP_HOST}:{settings.HTTP_PORT}"
+
     async def setUp(self):
         self.app = App()
         self.client = ClientSession()
@@ -23,13 +26,24 @@ class HTTPMetricsTests(TestCase):
             "asyncworker.metrics.aiohttp_resources.metrics"
         ).start()
 
-        @self.app.http._route(
-            routes=[self.route_path], method=self.route_method
-        )
+        @self.app.http.get(routes=[self.route_path])
         async def handler(wrapper: RequestWrapper):
             request = wrapper.http_request
             metrics.requests_in_progress.labels.assert_called_once_with(
                 method=request.method, path=request.path
+            )
+            metrics.requests_in_progress.labels.return_value.inc.assert_called_once()
+            metrics.requests_in_progress.labels.return_value.dec.assert_not_called()
+            await callback(request)
+            return Response(text="ok")
+
+        self.dynamic_route_path = "/resource/{id}"
+
+        @self.app.http.get(routes=[self.dynamic_route_path])
+        async def handler(wrapper: RequestWrapper):
+            request = wrapper.http_request
+            metrics.requests_in_progress.labels.assert_called_once_with(
+                method="GET", path=self.dynamic_route_path
             )
             metrics.requests_in_progress.labels.return_value.inc.assert_called_once()
             metrics.requests_in_progress.labels.return_value.dec.assert_not_called()
@@ -43,10 +57,7 @@ class HTTPMetricsTests(TestCase):
         patch.stopall()
 
     async def test_successful_request(self):
-        url = (
-            f"http://{settings.HTTP_HOST}:{settings.HTTP_PORT}{self.route_path}"
-        )
-
+        url = f"{self.app_url}{self.route_path}"
         async with self.client.get(url) as response:
             content = await response.text()
             self.assertEqual(response.status, HTTPStatus.OK)
@@ -69,9 +80,7 @@ class HTTPMetricsTests(TestCase):
         self.metrics.requests_in_progress.labels.return_value.dec.assert_called_once()
 
     async def test_unsuccessful_request(self):
-        url = (
-            f"http://{settings.HTTP_HOST}:{settings.HTTP_PORT}{self.route_path}"
-        )
+        url = f"{self.app_url}{self.route_path}"
         self.callback.side_effect = KeyError
         async with self.client.get(url) as response:
             await response.text()
@@ -91,9 +100,7 @@ class HTTPMetricsTests(TestCase):
         self.metrics.requests_in_progress.labels.return_value.dec.assert_called_once()
 
     async def test_notfound_request(self):
-        url = (
-            f"http://{settings.HTTP_HOST}:{settings.HTTP_PORT}{self.route_path}"
-        )
+        url = f"{self.app_url}{self.route_path}"
         self.callback.side_effect = HTTPNotFound
         async with self.client.get(url) as response:
             content = await response.text()
@@ -109,6 +116,29 @@ class HTTPMetricsTests(TestCase):
         )
         self.metrics.requests_in_progress.labels.assert_called_with(
             method=self.route_method, path=self.route_path
+        )
+        self.metrics.response_size.labels.return_value.observe.assert_called_once_with(
+            len(content)
+        )
+        self.metrics.request_duration.labels.return_value.observe.assert_called_once()
+        self.metrics.requests_in_progress.labels.return_value.dec.assert_called_once()
+
+    async def test_request_to_route_with_dynamic_path(self):
+        request_path = self.dynamic_route_path.format(id=uuid4().hex)
+
+        url = f"{self.app_url}{request_path}"
+        async with self.client.get(url) as response:
+            content = await response.text()
+            self.assertEqual(response.status, HTTPStatus.OK)
+
+        self.metrics.response_size.labels.assert_called_once_with(
+            method="GET", path=self.dynamic_route_path
+        )
+        self.metrics.request_duration.labels.assert_called_once_with(
+            method="GET", path=self.dynamic_route_path, status=response.status
+        )
+        self.metrics.requests_in_progress.labels.assert_called_with(
+            method="GET", path=self.dynamic_route_path
         )
         self.metrics.response_size.labels.return_value.observe.assert_called_once_with(
             len(content)
