f47488754d25b9dcc0335b6ff7081ce97549b6e5
Dalton Barreto <daltonmatos@gmail.com>
2021-04-13 21:01:45 -0300

    Simplificando a implementação do `@wraps()`

--- asyncworker/decorators.py
+++ asyncworker/decorators.py
@@ -14,12 +14,11 @@ def wraps(original_handler):
     """
 
     def _wrap(deco):
-        if hasattr(original_handler, "__original_annotations__"):
-            deco.__original_annotations__ = (
-                original_handler.__original_annotations__
-            )
-        else:
-            deco.__original_annotations__ = original_handler.__annotations__
+        deco.__original_annotations__ = getattr(
+            original_handler,
+            "__original_annotations__",
+            original_handler.__annotations__,
+        )
         return deco
 
     return _wrap
