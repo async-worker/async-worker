a84bb51774a503272321c0d6a6ae9a992349ea9f
Dalton Barreto <daltonmatos@gmail.com>
2021-04-18 13:03:23 -0300

    Criando clase base RequestParser
    Essa classe definirá a interface que todas as anotations precisarão
    implementar para que possam extrair partes do request.

--- asyncworker/http/types.py
+++ asyncworker/http/types.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Generic, TypeVar, Type
 
 from asyncworker.http.wrapper import RequestWrapper
@@ -5,14 +6,27 @@ from asyncworker.http.wrapper import RequestWrapper
 T = TypeVar("T")
 
 
-class PathParam(Generic[T]):
+class RequestParser(Generic[T]):
     def __init__(self, value: T) -> None:
         self._value: T = value
 
     @classmethod
+    @abstractmethod
     async def from_request(
         cls, request: RequestWrapper, arg_name: str, arg_type: Type
-    ) -> "PathParam":
+    ) -> "RequestParser[T]":
+        raise NotADirectoryError()
+
+    @abstractmethod
+    def unpack(self) -> T:
+        raise NotImplementedError()
+
+
+class PathParam(RequestParser[T]):
+    @classmethod
+    async def from_request(
+        cls, request: RequestWrapper, arg_name: str, arg_type: Type
+    ) -> "PathParam[T]":
         val = request.http_request.match_info[arg_name]
         return cls(arg_type(val))
 
