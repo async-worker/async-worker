663e1f250a0412eaa0580b89645d07a106fe7f79
Dalton Barreto <daltonmatos@gmail.com>
2021-04-23 23:17:09 -0300

    Transforma ValueError em HTTPBadRequest no PathParam

--- asyncworker/http/types.py
+++ asyncworker/http/types.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from typing import Generic, TypeVar, Type
 
+from aiohttp.web import HTTPBadRequest
+
 from asyncworker.http.wrapper import RequestWrapper
 
 T = TypeVar("T")
@@ -27,4 +29,7 @@ class PathParam(RequestParser[T]):
         cls, request: RequestWrapper, arg_name: str, arg_type: Type
     ) -> "PathParam[T]":
         val = request.http_request.match_info[arg_name]
-        return cls(arg_type(val))
+        try:
+            return cls(arg_type(val))
+        except ValueError as ve:
+            raise HTTPBadRequest(reason=ve.args[0])
